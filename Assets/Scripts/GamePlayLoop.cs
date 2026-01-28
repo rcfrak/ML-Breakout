c9d78a8c0c74324402ea454bcbb7c22596a07a55
stoutk <stoutk@oregonstate.edu>
2026-01-27 23:18:51 -0800

    Update GamePlayLoop.cs
    Omg. Well this is not the first thing I tried but... Because the bricks are instantiated, it's not easy make them reference or be referenced by another object (for example, calling a set method in another class to decrease a brick count). But there is a convenient way to count children of another game object through transform.childCount--you can count the number of brick clones generated by the level generator throughout the match simply this way.

--- Assets/Scripts/GamePlayLoop.cs
+++ Assets/Scripts/GamePlayLoop.cs
@@ -6,7 +6,10 @@ public class GamePlayLoop : MonoBehaviour
 {
     private Button restartButton;
     public GameObject breakoutBall;
+    public GameObject levelGenerator;
     public UIDocument uiDocument;
+
+    private int numBricks = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,16 +17,26 @@ public class GamePlayLoop : MonoBehaviour
         restartButton = uiDocument.rootVisualElement.Q<Button>("RestartButton");
         restartButton.style.display = DisplayStyle.None;
         restartButton.clicked += ReloadScene;
+
+        numBricks = levelGenerator.transform.childCount;
     }
 
     // Update is called once per frame
     void Update()
     {
+        numBricks = levelGenerator.transform.childCount;
+
         if (breakoutBall == null)
         {
             restartButton.style.display = DisplayStyle.Flex;
         }
+
+        if (numBricks == 0)
+        {
+            restartButton.style.display = DisplayStyle.Flex;
+        }
     }
+ 
 
     void ReloadScene()
     {
