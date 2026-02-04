df04609e30883ddf1d8658ef4b49389592c52cd3
Robert Fraker <frakerr@oregonstate.edu>
2026-02-03 21:23:21 -0500

    Updated PaddleMovements.cs
    Added border detection for paddle movement limits. Switched paddle speed to a public variable instead of hard coding value. Started using Rigidbody2D for physics as part of an attempt to reduce stuttering.

--- Assets/Scripts/PaddleMovement.cs
+++ Assets/Scripts/PaddleMovement.cs
@@ -1,26 +1,55 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class PaddleMovement : MonoBehaviour
 {
     public InputAction MoveAction;
-    public float left_boundary = -8f;
-    public float right_boundary = 8f;
+    private Rigidbody2D rigid_body;
+
+    public float speed = 8.00f;
+    private float left_boundary;
+    private float right_boundary;
+    private float half_paddle;
+    private float input;
+
+    // Experimenting with Rigidbody2D to help smooth physics and movement stuttering.
+    void Awake()
+    {
+        rigid_body = GetComponent<Rigidbody2D>();
+    }
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MoveAction.Enable();
+
+        // Locate the game objects in hierarchy
+        GameObject left_wall = GameObject.Find("Left_Wall");
+        GameObject right_wall = GameObject.Find("Right_Wall");
+        GameObject paddle = GameObject.Find("Paddle");
+
+        // Get the properties of the sprite to access the bounds
+        Bounds left_bounds = left_wall.GetComponent<SpriteRenderer>().bounds;
+        Bounds right_bounds = right_wall.GetComponent<SpriteRenderer>().bounds;
+        Bounds paddle_bounds = paddle.GetComponent<SpriteRenderer>().bounds;
+
+        // Set paddle boundaries based on the left and right walls, taking into account the paddle size
+        half_paddle = paddle_bounds.extents.x;
+        left_boundary = left_bounds.max.x + half_paddle;
+        right_boundary = right_bounds.min.x - half_paddle;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Switched from Update in an attempt to smooth the physics and movement stuttering.
+    // Seems slightly better on my PC. Not sure if it is a major improvement.
+    void FixedUpdate()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
-        //Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 8.00f * Time.deltaTime; // Change paddle position with move input, speed, per second for frames.
-        position.x = Mathf.Clamp(position.x, left_boundary, right_boundary);  // Lock paddle in play space.
-        transform.position = position;
+        input = MoveAction.ReadValue<Vector2>().x;
+
+        Vector2 target = rigid_body.position;
+        target.x += input * speed * Time.fixedDeltaTime;
+        target.x = Mathf.Clamp(target.x, left_boundary, right_boundary);
+
+        rigid_body.MovePosition(target);
     }
 }
